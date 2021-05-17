ca48dc2bb8f59a214f5b85d58901b149daffcdd1
Matthew Stinis <mpstinis@gmail.com>
2021-05-17 16:20:50 -0400

    [App] frontend reading data from flask server successfully

--- src/Pages/CommentsPage.jsx
+++ src/Pages/CommentsPage.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from "../Components/Card";
 
 const CommentsPage = () => {
+
+    const [comment, setComment] = useState([])
+
+    useEffect(() => {
+        try {
+            fetch('/api').then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+            }).then(data => console.log(data))
+        } catch (error) {
+            console.error(error)
+        }
+
+    }, [])
+
     return (
         <div>
             <Card />
